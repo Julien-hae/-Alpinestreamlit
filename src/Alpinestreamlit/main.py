4e5d7d1e0001660804cf094f6dacf745b8d25e69
Julien Haefelin <julien.haefelin@gmail.com>
2024-12-18 15:59:12 +0100

    feat: Configure repository for minimal use

--- src/Alpinestreamlit/main.py
+++ src/Alpinestreamlit/main.py
@@ -1,35 +1,19 @@
 """Main Description."""
 
-import argparse
 import logging
 
-import numpy as np
-import numpy.typing as npt
-
-from Alpinestreamlit import greeter
+import streamlit as st
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_np_array() -> npt.NDArray[np.int_]:
-    """Return a numpy array."""
-    return np.array([0])
-
-
-def main(word: str) -> None:
+def main() -> None:
     """Main entry point for the application."""
-    LOGGER.info("Executing entrypoint.")
-    print(greeter.Greeter(word).get_greeting())
-    print(f"Here's a test that poetry dependencies are installed: {get_np_array()}")
-
-
-def cli() -> None:
-    """Cli-Entrypoint."""
-    parser = argparse.ArgumentParser(description="Demo argument")
-    parser.add_argument("-w", "--word", required=True)
-    args = parser.parse_args()
-    main(args.word)
+    LOGGER.info("Executing Streamlit Application.")
+    pressed = st.button("press me")
+    if pressed:
+        st.title("Hello SBB!")
 
 
 if __name__ == "__main__":
-    cli()
+    main()
